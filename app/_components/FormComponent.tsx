528e245dc6a8ac71e78426a0e2db8fc24c134dc5
Simon Tran <thanhnhantran1501@gmail.com>
2023-12-20 15:13:12 -0800

    feat: create state for form using useState and a function to update the state

--- app/_components/FormComponent.tsx
+++ app/_components/FormComponent.tsx
@@ -1,8 +1,39 @@
 "use client";
 import React from "react";
+import { useState } from "react";
 import InputComponent from "./InputComponent";
 
+interface FormInfor {
+  username: string;
+  email: string;
+  message: string;
+}
+
 export default function FormComponent() {
+  const [formInfor, setFormInfor] = useState<FormInfor>({
+    username: "",
+    email: "",
+    message: "",
+  });
+
+  const handleUpdateFormInfor = function (
+    field: "username" | "email" | "message",
+    val: string
+  ) {
+    setFormInfor((prevState: FormInfor) => {
+      const newForm = { ...prevState };
+      newForm[field] = val;
+      return newForm;
+    });
+  };
+
+  const inputBoxesInforArr = [
+    {
+      name: "username",
+    },
+    { name: "email" },
+    { name: "message" },
+  ];
   return (
     <div>
       <InputComponent />
