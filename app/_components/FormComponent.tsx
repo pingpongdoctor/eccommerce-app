1f8c13a182bbce9245f115627b7ca40ae49953f7
Simon Tran <thanhnhantran1501@gmail.com>
2023-12-20 14:48:34 -0800

    style: design an reusable input component

---
+++ app/_components/FormComponent.tsx
@@ -0,0 +1,11 @@
+"use client";
+import React from "react";
+import InputComponent from "./InputComponent";
+
+export default function FormComponent() {
+  return (
+    <div>
+      <InputComponent />
+    </div>
+  );
+}
