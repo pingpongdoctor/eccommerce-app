939bed0efebd781905b084e06e62967dcee87088
Simon Tran <thanhnhantran1501@gmail.com>
2023-12-20 21:50:32 -0800

    feat: add animation to the button

--- app/_components/ButtonComponent.tsx
+++ app/_components/ButtonComponent.tsx
@@ -8,10 +8,13 @@ interface Props {
 export default function ButtonComponent({ buttonName, buttonType }: Props) {
   return (
     <button
-      className="px-8 py-4 rounded-md font-bold text-white bg-black hover:scale-[1.03] duration-300"
+      className="group w-[100px] h-[50px] rounded-md font-bold transition-all hover:scale-[1.03 relative"
       type={buttonType}
     >
-      {buttonName}
+      <span className="absolute rounded-md z-0 left-0 top-0 w-[2px] h-full bg-black transition-all group-hover:w-full flex items-center justify-center"></span>
+      <span className="absolute rounded-md left-0 top-0 flex items-center justify-center transition-all w-full h-full text-black group-hover:text-white">
+        <p>{buttonName}</p>
+      </span>
     </button>
   );
 }
