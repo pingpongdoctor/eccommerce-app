ebc6b9727a01f8e5d99f02fe19fe1dc8a07834be
Simon Tran <thanhnhantran1501@gmail.com>
2023-12-21 22:42:59 -0800

    feat: apply time-based revalidation for the function getAllUserData to revalidate user data every 1 hour

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import { notFound } from "next/navigation";
 import FormComponent from "./_components/FormComponent";
 import TableComponent from "./_components/TableComponent";
 import { getAllUserData } from "./_lib/getAllUserData";
+
 export default async function Home() {
   const data = await getAllUserData();
 
