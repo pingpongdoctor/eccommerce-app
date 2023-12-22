3a94b9ea7114200541447289f5378957cfafd68a
Simon Tran <thanhnhantran1501@gmail.com>
2023-12-22 11:39:35 -0800

    feat: add prettier tailwind plugin to sort utility classes

--- app/icon.tsx
+++ app/icon.tsx
@@ -1,16 +1,13 @@
 import { ImageResponse } from "next/og";
 
-// Route segment config
 export const runtime = "edge";
 
-// Image metadata
 export const size = {
   width: 38,
   height: 32,
 };
 export const contentType = "image/png";
 
-// Image generation
 export default function Icon() {
   return new ImageResponse(
     (
