8151fe48bbb84fa36805185c26dea7f24d4dede0
Simon Tran <thanhnhantran1501@gmail.com>
2023-12-25 15:56:32 -0800

    feat: add page for single post and add tailwind typography plugin

---
+++ app/_components/Post.tsx
@@ -0,0 +1,31 @@
+import { SanityDocument } from "next-sanity";
+import Image from "next/image";
+import { PortableText } from "@portabletext/react";
+import { dataset, projectId } from "@/sanity/env";
+import imageUrlBuilder from "@sanity/image-url";
+
+const builder = imageUrlBuilder({ projectId, dataset });
+
+export default function Post({ post }: { post: SanityDocument }) {
+  const { title, mainImage, body } = post;
+  return (
+    <main className="prose prose-lg container mx-auto p-4">
+      {title ? <h1>{title}</h1> : null}
+      {mainImage ? (
+        <Image
+          className="float-left m-0 mr-4 w-1/3 rounded-lg"
+          src={builder
+            .image(mainImage)
+            .width(300)
+            .height(300)
+            .quality(80)
+            .url()}
+          width={300}
+          height={300}
+          alt={mainImage.alt || ""}
+        />
+      ) : null}
+      {body ? <PortableText value={body} /> : null}
+    </main>
+  );
+}
---
+++ app/_components/PostPreview.tsx
@@ -0,0 +1,27 @@
+// ./components/PostPreview.tsx
+
+"use client";
+
+import { POST_QUERY } from "@/sanity/lib/queries";
+import { QueryResponseInitial, useQuery } from "@sanity/react-loader";
+import { QueryParams, SanityDocument } from "next-sanity";
+
+import Post from "./Post";
+
+export default function PostsPreview({
+  initial,
+  params,
+}: {
+  initial: QueryResponseInitial<SanityDocument>;
+  params: QueryParams;
+}) {
+  const { data } = useQuery<SanityDocument | null>(POST_QUERY, params, {
+    initial,
+  });
+
+  return data ? (
+    <Post post={data} />
+  ) : (
+    <div className="bg-red-100">No post found</div>
+  );
+}
