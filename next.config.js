565c00eeace441060028540cfac5af7f570f0779
Simon Tran <thanhnhantran1501@gmail.com>
2023-12-24 22:51:22 -0800

    feat: set up next.config.js for using sanity image and rendering token on server side

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+      },
+    ],
+  },
+  experimental: {
+    taint: true,
+  },
+  // ...other config settings
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
