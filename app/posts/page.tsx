95c5d2a79790ee8a1e8146d1c733719f57a3d270
Simon Tran <thanhnhantran1501@gmail.com>
2023-12-25 11:41:33 -0800

    feat: create qyeries for posts and post

---
+++ app/posts/page.tsx
@@ -0,0 +1,10 @@
+import { SanityDocument } from "next-sanity";
+import Posts from "../_components/Posts";
+import { loadQuery } from "@/sanity/lib/store";
+import { POSTS_QUERY } from "@/sanity/lib/queries";
+
+export default async function Page() {
+  const initial = await loadQuery<SanityDocument[]>(POSTS_QUERY);
+
+  return <Posts posts={initial.data} />;
+}
