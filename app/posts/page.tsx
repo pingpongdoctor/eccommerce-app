46a75f3381b21992004a4a89458efb45a20ef292
Simon Tran <thanhnhantran1501@gmail.com>
2023-12-25 14:49:16 -0800

    refactor: refactor the post page to show either posts or postspreview pages depending on whether draft mode is enabled

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -2,9 +2,21 @@ import { SanityDocument } from "next-sanity";
 import Posts from "../_components/Posts";
 import { loadQuery } from "@/sanity/lib/store";
 import { POSTS_QUERY } from "@/sanity/lib/queries";
+import { draftMode } from "next/headers";
+import PostsPreview from "../_components/PostsPreview";
 
 export default async function Page() {
-  const initial = await loadQuery<SanityDocument[]>(POSTS_QUERY);
+  const initial = await loadQuery<SanityDocument[]>(
+    POSTS_QUERY,
+    {},
+    {
+      perspective: draftMode().isEnabled ? "previewDrafts" : "published",
+    },
+  );
 
-  return <Posts posts={initial.data} />;
+  return draftMode().isEnabled ? (
+    <PostsPreview initial={initial} />
+  ) : (
+    <Posts posts={initial.data} />
+  );
 }
