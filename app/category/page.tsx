a680f8e1705e3862eb1b6c20e1df7be622352a20
Simon Tran <thanhnhantran1501@gmail.com>
2023-12-26 13:11:43 -0800

    feat: add glowing stars ui

--- app/category/page.tsx
+++ app/category/page.tsx
@@ -1,27 +1,13 @@
-import { SanityDocument } from "next-sanity";
-import Products from "../_components/ProductCards";
-import { loadQuery } from "@/sanity/lib/store";
-import { PRODUCTS_QUERY } from "@/sanity/lib/queries";
-import { draftMode } from "next/headers";
-import ProductCardsPreview from "../_components/ProductCardsPreview";
-import { notFound } from "next/navigation";
+// import { SanityDocument } from "next-sanity";
+// import Products from "../_components/ProductCards";
+// import { loadQuery } from "@/sanity/lib/store";
+// import { PRODUCTS_QUERY } from "@/sanity/lib/queries";
+// import { draftMode } from "next/headers";
+// import ProductCardsPreview from "../_components/ProductCardsPreview";
+// import { notFound } from "next/navigation";
+import React from "react";
+import { CategoryCard } from "../_components/CategoryCard";
 
-export default async function Categories() {
-  const initial = await loadQuery<SanityDocument[]>(
-    PRODUCTS_QUERY,
-    {},
-    {
-      perspective: draftMode().isEnabled ? "previewDrafts" : "published",
-    },
-  );
-
-  if (!initial.data) {
-    notFound();
-  }
-
-  return draftMode().isEnabled ? (
-    <ProductCardsPreview initial={initial} />
-  ) : (
-    <Products products={initial.data} />
-  );
+export default function Categories() {
+  return <CategoryCard />;
 }
--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -4,4 +4,4 @@ export const PRODUCTS_QUERY = groq`*[_type == "post" && defined(slug)]`;
 
 export const PRODUCT_QUERY = groq`*[_type == "post" && slug.current == $slug][0]`;
 
-export const PRODUCT_QUERY_BASED_CATEGORY = groq`*[_type == "post" && slug.current == $slug][0]`;
+export const PRODUCT_QUERY_BASED_CATEGORY = groq`*[_type == "post" && defined(category)]`;
