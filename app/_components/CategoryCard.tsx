a7abd89e8ee4a488f5279ecf6a3076070c72f48f
Simon Tran <thanhnhantran1501@gmail.com>
2023-12-26 14:48:23 -0800

    feat: design category page

--- app/_components/CategoryCard.tsx
+++ app/_components/CategoryCard.tsx
@@ -1,44 +1,176 @@
 "use client";
-import React from "react";
-import {
-  GlowingStarsBackgroundCard,
-  GlowingStarsDescription,
-  GlowingStarsTitle,
-} from "./GlowingStarsUI";
+import React, { useEffect, useRef, useState, memo } from "react";
+import { motion } from "framer-motion";
+import { twMerge } from "tailwind-merge";
+import { cn } from "../utils/cs";
 
-export function CategoryCard() {
+export const CategoryCard = ({
+  text,
+  revealText,
+  children,
+  className,
+}: {
+  text: string;
+  revealText: string;
+  children?: React.ReactNode;
+  className?: string;
+}) => {
+  const [widthPercentage, setWidthPercentage] = useState(0);
+  const cardRef = useRef<HTMLDivElement | any>(null);
+  const [left, setLeft] = useState(0);
+  const [localWidth, setLocalWidth] = useState(0);
+  const [isMouseOver, setIsMouseOver] = useState(false);
+
+  useEffect(() => {
+    if (cardRef.current) {
+      const { left, width: localWidth } =
+        cardRef.current.getBoundingClientRect();
+      setLeft(left);
+      setLocalWidth(localWidth);
+    }
+  }, []);
+
+  function mouseMoveHandler(event: any) {
+    event.preventDefault();
+
+    const { clientX } = event;
+    if (cardRef.current) {
+      const relativeX = clientX - left;
+      setWidthPercentage((relativeX / localWidth) * 100);
+    }
+  }
+
+  function mouseLeaveHandler() {
+    setIsMouseOver(false);
+    setWidthPercentage(0);
+  }
+  function mouseEnterHandler() {
+    setIsMouseOver(true);
+  }
+
+  const rotateDeg = (widthPercentage - 50) * 0.1;
   return (
-    <div className="flex items-center justify-center py-20 antialiased [&_h2]:m-0">
-      <GlowingStarsBackgroundCard>
-        <GlowingStarsTitle>Comestic</GlowingStarsTitle>
-        <div className="flex items-end justify-between">
-          <GlowingStarsDescription>
-            Discover the best comestic products ever in Canada
-          </GlowingStarsDescription>
-          <div className="flex h-8 w-8 items-center justify-center rounded-full bg-[hsla(0,0%,100%,.1)]">
-            <Icon />
-          </div>
+    <div
+      onMouseEnter={mouseEnterHandler}
+      onMouseLeave={mouseLeaveHandler}
+      onMouseMove={mouseMoveHandler}
+      ref={cardRef}
+      className={cn(
+        "relative w-[40rem] overflow-hidden rounded-lg border border-white/[0.08] bg-[#1d1c20] p-8",
+        className,
+      )}
+    >
+      {children}
+
+      <div className="relative  flex h-40 items-center overflow-hidden">
+        <motion.div
+          style={{
+            width: "100%",
+          }}
+          animate={
+            isMouseOver
+              ? {
+                  opacity: widthPercentage > 0 ? 1 : 0,
+                  clipPath: `inset(0 ${100 - widthPercentage}% 0 0)`,
+                }
+              : {
+                  clipPath: `inset(0 ${100 - widthPercentage}% 0 0)`,
+                }
+          }
+          transition={isMouseOver ? { duration: 0 } : { duration: 0.4 }}
+          className="absolute z-20 bg-[#1d1c20]  will-change-transform"
+        >
+          <p
+            style={{
+              textShadow: "4px 4px 15px rgba(0,0,0,0.5)",
+            }}
+            className="bg-gradient-to-b from-white to-neutral-300 bg-clip-text py-10 text-base font-bold text-transparent text-white sm:text-[3rem]"
+          >
+            {revealText}
+          </p>
+        </motion.div>
+        <motion.div
+          animate={{
+            left: `${widthPercentage}%`,
+            rotate: `${rotateDeg}deg`,
+            opacity: widthPercentage > 0 ? 1 : 0,
+          }}
+          transition={isMouseOver ? { duration: 0 } : { duration: 0.4 }}
+          className="absolute z-50 h-40 w-[8px] bg-gradient-to-b from-transparent via-neutral-800 to-transparent will-change-transform"
+        ></motion.div>
+
+        <div className=" overflow-hidden [mask-image:linear-gradient(to_bottom,transparent,white,transparent)]">
+          <p className="bg-[#323238] bg-clip-text py-10 text-base font-bold text-transparent sm:text-[3rem]">
+            {text}
+          </p>
+          <MemoizedStars />
         </div>
-      </GlowingStarsBackgroundCard>
+      </div>
     </div>
   );
-}
+};
 
-const Icon = () => {
+export const TextRevealCardTitle = ({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) => {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke-width="1.5"
-      stroke="currentColor"
-      className="h-4 w-4 stroke-2 text-white"
-    >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
-      />
-    </svg>
+    <h2 className={twMerge("mb-2 text-lg text-white", className)}>
+      {children}
+    </h2>
   );
 };
+
+export const TextRevealCardDescription = ({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) => {
+  return (
+    <p className={twMerge("text-sm text-[#a9a9a9]", className)}>{children}</p>
+  );
+};
+
+const Stars = () => {
+  const randomMove = () => Math.random() * 4 - 2;
+  const randomOpacity = () => Math.random();
+  const random = () => Math.random();
+  return (
+    <div className="absolute inset-0">
+      {[...Array(140)].map((_, i) => (
+        <motion.span
+          key={`star-${i}`}
+          animate={{
+            top: `calc(${random() * 100}% + ${randomMove()}px)`,
+            left: `calc(${random() * 100}% + ${randomMove()}px)`,
+            opacity: randomOpacity(),
+            scale: [1, 1.2, 0],
+          }}
+          transition={{
+            duration: random() * 10 + 20,
+            repeat: Infinity,
+            ease: "linear",
+          }}
+          style={{
+            position: "absolute",
+            top: `${random() * 100}%`,
+            left: `${random() * 100}%`,
+            width: `2px`,
+            height: `2px`,
+            backgroundColor: "white",
+            borderRadius: "50%",
+            zIndex: 1,
+          }}
+          className="inline-block"
+        ></motion.span>
+      ))}
+    </div>
+  );
+};
+
+export const MemoizedStars = memo(Stars);
--- app/category/page.tsx
+++ app/category/page.tsx
@@ -1,13 +1,61 @@
-// import { SanityDocument } from "next-sanity";
-// import Products from "../_components/ProductCards";
-// import { loadQuery } from "@/sanity/lib/store";
-// import { PRODUCTS_QUERY } from "@/sanity/lib/queries";
-// import { draftMode } from "next/headers";
-// import ProductCardsPreview from "../_components/ProductCardsPreview";
-// import { notFound } from "next/navigation";
 import React from "react";
 import { CategoryCard } from "../_components/CategoryCard";
+import Link from "next/link";
+
+const categoryInfor: {
+  id: string;
+  text: string;
+  revealText: string;
+  className: string;
+}[] = [
+  {
+    id: "comestic",
+    text: "Comestic Collection",
+    revealText: "Discover Comestic Products",
+    className: "w-full rounded-xl [&_p]:text-3xl sm:[&_p]:text-4xl",
+  },
+  {
+    id: "supplement",
+    text: "Supplement Products",
+    revealText: "Find Your Wanted Supplements",
+    className: "w-full rounded-xl [&_p]:text-3xl sm:[&_p]:text-4xl",
+  },
+  {
+    id: "food",
+    text: "Canadian Food",
+    revealText: "Enjoy Canadian Food",
+    className: "w-full rounded-xl [&_p]:text-3xl sm:[&_p]:text-4xl",
+  },
+  {
+    id: "other",
+    text: "Other Merchandises",
+    revealText: "What ever you find can be here",
+    className: "w-full rounded-xl [&_p]:text-3xl sm:[&_p]:text-4xl",
+  },
+];
 
 export default function Categories() {
-  return <CategoryCard />;
+  return (
+    <main className="m-4 sm:m-8">
+      <ul className="flex list-none flex-col gap-4 sm:w-full sm:flex-row sm:flex-wrap sm:gap-8">
+        {categoryInfor.map((category) => {
+          return (
+            <Link
+              key={category.id}
+              href={`/category/${category.id}`}
+              className="w-full rounded-xl sm:w-[calc((100%-2rem)/2)]"
+            >
+              <li>
+                <CategoryCard
+                  text={category.text}
+                  revealText={category.revealText}
+                  className={category.className}
+                />
+              </li>
+            </Link>
+          );
+        })}
+      </ul>
+    </main>
+  );
 }
