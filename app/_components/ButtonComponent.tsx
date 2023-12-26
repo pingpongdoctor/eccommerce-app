a4fc61a116728f316041622995566e6aad6742a5
Simon Tran <thanhnhantran1501@gmail.com>
2023-12-25 23:35:48 -0800

    feat: add color and function props for button

--- app/_components/ButtonComponent.tsx
+++ app/_components/ButtonComponent.tsx
@@ -4,26 +4,43 @@ import React from "react";
 
 interface Props {
   buttonName: string;
-  buttonType: "submit" | "reset" | "button" | undefined;
+  buttonType?: "submit" | "reset" | "button" | undefined;
   isDisabled?: boolean;
+  buttonOnclickHandler?: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  buttonColor?: "blue" | "black";
 }
+
+const bgColors: { [index: string]: string } = {
+  blue: "bg-blue-700",
+  black: "bg-black",
+};
+
+const borderColors: { [index: string]: string } = {
+  blue: "border-blue-700",
+  black: "border-black",
+};
 
 export default function ButtonComponent({
   buttonName,
   buttonType,
   isDisabled = false,
+  buttonOnclickHandler,
+  buttonColor = "black",
 }: Props) {
   return (
     <button
-      className="group w-[100px] h-[50px] border border-black rounded-md font-bold relative"
+      className={`group relative h-[50px] w-[100px] rounded-md border-[1.5px] font-bold ${borderColors[buttonColor]}`}
       type={buttonType}
       disabled={isDisabled}
+      onClick={buttonOnclickHandler}
     >
-      <span className="absolute rounded-md left-0 top-0 w-1 h-full bg-black transition-all group-hover:w-full group-disabled:hidden flex items-center justify-center"></span>
-      <span className="absolute rounded-md left-0 top-0 flex items-center justify-center transition-all w-full h-full text-black group-hover:text-white group-disabled:hidden">
+      <span
+        className={`absolute left-0 top-0 flex h-full w-1 rounded ${bgColors[buttonColor]} transition-all group-hover:w-full group-disabled:hidden`}
+      ></span>
+      <span className="absolute left-0 top-0 flex h-full w-full items-center justify-center rounded-md text-black transition-all group-hover:text-white group-disabled:hidden">
         <p>{buttonName}</p>
       </span>
-      <span className="absolute rounded-md hidden left-0 top-0 w-full h-full group-disabled:flex justify-center items-center">
+      <span className="absolute left-0 top-0 hidden h-full w-full items-center justify-center rounded-md group-disabled:flex">
         <ThreeDots
           visible={true}
           height="50"
