8e0ab2c29df789487657612b51167def1ac59f38
Simon Tran <thanhnhantran1501@gmail.com>
2023-12-26 19:12:59 -0800

    feat: setup tailwind element react plugin

--- app/_components/GlowingStarsCard.tsx
+++ app/_components/GlowingStarsCard.tsx
@@ -30,7 +30,7 @@ const Icon = () => {
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
       viewBox="0 0 24 24"
-      stroke-width="1.5"
+      strokeWidth="1.5"
       stroke="currentColor"
       className="h-4 w-4 stroke-2 text-white"
     >
