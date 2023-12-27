5ed5f9c677d2f6a75d8064feafe9d6ff605c2b85
Simon Tran <thanhnhantran1501@gmail.com>
2023-12-26 23:01:11 -0800

    feat: design image carousel and image gallery for product detail page

--- app/_components/Carousel.tsx
+++ app/_components/Carousel.tsx
@@ -1,123 +1,52 @@
+"use client";
+
 import React from "react";
+import dynamic from "next/dynamic";
 import Image from "next/image";
 
+const TECarousel = dynamic(() =>
+  import("tw-elements-react").then((res) => res.TECarousel),
+);
+const TECarouselItem = dynamic(() =>
+  import("tw-elements-react").then((res) => res.TECarouselItem),
+);
+
 interface Props {
-  images: string[];
+  images: (ImageInfor & { id: number; imageLink: string })[];
   carouselClassname?: string;
 }
 
 export default function Carousel({ images, carouselClassname }: Props) {
+  console.log(images);
   return (
-    <div
-      id="default-carousel"
-      className={`relative w-full ${carouselClassname}`}
-      data-carousel="slide"
+    <TECarousel
+      showControls
+      showIndicators
+      ride="carousel"
+      className="w-[500px]"
     >
-      <div className="relative h-56 overflow-hidden rounded-lg md:h-96">
+      <div className="relative w-full overflow-hidden after:clear-both after:block after:content-['']">
         {images?.length > 0 &&
-          images.map((image: string, index: number) => (
-            <div
-              key={index}
-              className="duration-700 ease-in-out"
-              data-carousel-item
-            >
-              <Image
-                width={300}
-                height={300}
-                src={image}
-                className="absolute left-1/2 top-1/2 block w-full -translate-x-1/2 -translate-y-1/2"
-                alt="..."
-              />
-            </div>
-          ))}
+          images.map(
+            (image: ImageInfor & { id: number; imageLink: string }) => {
+              return (
+                <TECarouselItem
+                  key={image._key}
+                  itemID={image.id}
+                  className="relative float-left -mr-[100%] hidden w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
+                >
+                  <Image
+                    src={image.imageLink}
+                    className="block h-[500px] w-full object-cover"
+                    alt={image._type}
+                    width={300}
+                    height={300}
+                  />
+                </TECarouselItem>
+              );
+            },
+          )}
       </div>
-
-      <div className="absolute bottom-5 left-1/2 z-30 flex -translate-x-1/2 space-x-3 rtl:space-x-reverse">
-        <button
-          type="button"
-          className="h-3 w-3 rounded-full"
-          aria-current="true"
-          aria-label="Slide 1"
-          data-carousel-slide-to="0"
-        ></button>
-        <button
-          type="button"
-          className="h-3 w-3 rounded-full"
-          aria-current="false"
-          aria-label="Slide 2"
-          data-carousel-slide-to="1"
-        ></button>
-        <button
-          type="button"
-          className="h-3 w-3 rounded-full"
-          aria-current="false"
-          aria-label="Slide 3"
-          data-carousel-slide-to="2"
-        ></button>
-        <button
-          type="button"
-          className="h-3 w-3 rounded-full"
-          aria-current="false"
-          aria-label="Slide 4"
-          data-carousel-slide-to="3"
-        ></button>
-        <button
-          type="button"
-          className="h-3 w-3 rounded-full"
-          aria-current="false"
-          aria-label="Slide 5"
-          data-carousel-slide-to="4"
-        ></button>
-      </div>
-
-      <button
-        type="button"
-        className="group absolute start-0 top-0 z-30 flex h-full cursor-pointer items-center justify-center px-4 focus:outline-none"
-        data-carousel-prev
-      >
-        <span className="inline-flex h-10 w-10 items-center justify-center rounded-full bg-white/30 group-hover:bg-white/50 group-focus:outline-none group-focus:ring-4 group-focus:ring-white dark:bg-gray-800/30 dark:group-hover:bg-gray-800/60 dark:group-focus:ring-gray-800/70">
-          <svg
-            className="h-4 w-4 text-white rtl:rotate-180 dark:text-gray-800"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 6 10"
-          >
-            <path
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M5 1 1 5l4 4"
-            />
-          </svg>
-          <span className="sr-only">Previous</span>
-        </span>
-      </button>
-      <button
-        type="button"
-        className="group absolute end-0 top-0 z-30 flex h-full cursor-pointer items-center justify-center px-4 focus:outline-none"
-        data-carousel-next
-      >
-        <span className="inline-flex h-10 w-10 items-center justify-center rounded-full bg-white/30 group-hover:bg-white/50 group-focus:outline-none group-focus:ring-4 group-focus:ring-white dark:bg-gray-800/30 dark:group-hover:bg-gray-800/60 dark:group-focus:ring-gray-800/70">
-          <svg
-            className="h-4 w-4 text-white rtl:rotate-180 dark:text-gray-800"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 6 10"
-          >
-            <path
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="m1 9 4-4-4-4"
-            />
-          </svg>
-          <span className="sr-only">Next</span>
-        </span>
-      </button>
-    </div>
+    </TECarousel>
   );
 }
