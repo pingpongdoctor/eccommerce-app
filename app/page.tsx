b8310957f506089b8130e87ee59ba328a9e883df
Simon Tran <thanhnhantran1501@gmail.com>
2023-12-28 14:29:03 -0800

    fix: log baseUrl to fix bugs

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,11 @@ import { notFound } from "next/navigation";
 import FormComponent from "./_components/FormComponent";
 import TableComponent from "./_components/TableComponent";
 import { getAllUserData } from "./_lib/getAllUserData";
+import { baseUrl } from "./utils/baseUrl";
 
 export default async function Home() {
   const data = await getAllUserData();
+  console.log(baseUrl);
 
   if (!data) {
     notFound();
