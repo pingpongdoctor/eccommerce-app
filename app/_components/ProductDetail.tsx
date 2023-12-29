c0316bcba381f8dea1c96de0bb978957d9a691c6
Simon Tran <thanhnhantran1501@gmail.com>
2023-12-29 01:23:31 -0800

    feat: add rich text image component

--- app/_components/ProductDetail.tsx
+++ app/_components/ProductDetail.tsx
@@ -2,26 +2,30 @@ import { SanityDocument } from 'next-sanity';
 import Image from 'next/image';
 import { PortableText } from '@portabletext/react';
 import { builder } from '../utils/imageBuilder';
+import CarouselComponent from './CarouselComponent';
+import RichTextImageComponent from './RichTextImageComponent';
 
 export default function Product({ product }: { product: SanityDocument }) {
   const { title, mainImage, body } = product;
 
-  const addIdNumberToImage = function (
-    imgArr: (ImageInfor & { id?: number; imageLink?: string })[]
-  ) {
-    let idVal = 1;
-    for (let i = 0; i < imgArr.length; i++) {
-      imgArr[i].id = idVal;
-      imgArr[i].imageLink = builder.image(imgArr[i].asset).quality(80).url();
-      idVal += 1;
-    }
-    return imgArr;
-  };
+  // const addIdNumberToImage = function (
+  //   imgArr: (ImageInfor & { id?: number; imageLink?: string })[],
+  // ) {
+  //   let idVal = 1;
+  //   for (let i = 0; i < imgArr.length; i++) {
+  //     imgArr[i].id = idVal;
+  //     imgArr[i].imageLink = builder.image(imgArr[i].asset).quality(80).url();
+  //     idVal += 1;
+  //   }
+  //   return imgArr;
+  // };
 
   return (
     <main className="m-4 bg-dot-black/[0.2] sm:m-8">
       {mainImage?.length > 0 ? (
         <div>
+          <CarouselComponent />
+
           <ul className="hidden max-w-[700px] list-none sm:flex sm:flex-wrap sm:gap-4">
             {mainImage.map((image: ImageInfor) => {
               return (
@@ -42,7 +46,17 @@ export default function Product({ product }: { product: SanityDocument }) {
           </ul>
         </div>
       ) : null}
-      {body ? <PortableText value={body} /> : null}
+      {body ? (
+        <PortableText
+          value={body}
+          components={{
+            // ...
+            types: {
+              image: RichTextImageComponent,
+            },
+          }}
+        />
+      ) : null}
     </main>
   );
 }
