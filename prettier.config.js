794858612566b53eb814be66e7ab04e2369f2676
Simon Tran <thanhnhantran1501@gmail.com>
2023-12-29 22:06:07 -0800

    chores: delete duplicate prettier config file

--- prettier.config.js
+++ prettier.config.js
@@ -1,7 +1,7 @@
 // prettier.config.js
 module.exports = {
-  plugins: ["prettier-plugin-tailwindcss"],
-  trailingComma: "es5",
+  plugins: ['prettier-plugin-tailwindcss'],
+  trailingComma: 'es5',
   semi: true,
   singleQuote: true,
 };
