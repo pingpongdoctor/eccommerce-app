ef4b3daadb952cfe4fda11ef9cc2a26d0921f631
Simon Tran <thanhnhantran1501@gmail.com>
2023-12-30 02:08:29 -0800

    refactor: change icon name

--- app/icon.tsx
+++ app/icon.tsx
@@ -1,12 +1,12 @@
-import { ImageResponse } from "next/og";
+import { ImageResponse } from 'next/og';
 
-export const runtime = "edge";
+export const runtime = 'edge';
 
 export const size = {
   width: 38,
   height: 32,
 };
-export const contentType = "image/png";
+export const contentType = 'image/png';
 
 export default function Icon() {
   return new ImageResponse(
@@ -15,20 +15,20 @@ export default function Icon() {
       <div
         style={{
           fontSize: 12,
-          background: "black",
-          width: "100%",
-          height: "100%",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          color: "white",
+          background: 'black',
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          color: 'white',
         }}
       >
-        Thao
+        Ecommerce
       </div>
     ),
     {
       ...size,
-    },
+    }
   );
 }
