de74538dcaba5a9324c49f5c9ee914825131d136
Simon Tran <thanhnhantran1501@gmail.com>
2024-01-02 10:49:18 -0800

    feat: add more global styles using Tailwind base directive

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,9 @@
-import { notFound } from 'next/navigation';
-import FormComponent from './_components/FormComponent';
-import TableComponent from './_components/TableComponent';
-import { getAllUserData } from './_lib/getAllUserData';
+import HeroSection from './_components/HeroSection';
 
 export default async function Home() {
-  const data = await getAllUserData();
-
-  if (!data) {
-    notFound();
-  }
-
   return (
-    <main className="mx-2 my-4 sm:mx-6 sm:my-8">
-      <FormComponent />
-      <TableComponent users={data} />
+    <main className="p-4 md:p-8">
+      <HeroSection />
     </main>
   );
 }
