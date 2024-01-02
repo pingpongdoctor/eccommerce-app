b9a123774319134d542123c342cac00fc6780dcb
Simon Tran <thanhnhantran1501@gmail.com>
2024-01-02 14:11:56 -0800

    feat: add hero sections

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,12 @@ import HeroSection from './_components/HeroSection';
 
 export default async function Home() {
   return (
-    <main className="p-4 md:p-8">
+    <main className="p-4 md:p-8 lg:p-12">
+      <div className="mb-16 flex h-[80dvh] w-full items-center rounded-3xl bg-[url('/assets/pexels-scott-webb-403571.jpg')] bg-cover bg-fixed bg-[50%_13%] bg-no-repeat">
+        <h1 className="ml-8 font-dancingScript text-6xl">
+          Welcome to the Glowy Lab Family
+        </h1>
+      </div>
       <HeroSection />
     </main>
   );
