f6d0e3a588c485ec43dfff75b5748fd12c749b85
Simon Tran <thanhnhantran1501@gmail.com>
2024-01-03 00:13:19 -0800

    feat: add design for carousel in mobile breakpoint

--- app/_components/BlogCard.tsx
+++ app/_components/BlogCard.tsx
@@ -4,10 +4,10 @@ import Avatar from './Avatar';
 
 export default function BlogCard() {
   return (
-    <div className="aspect-[1/1.15] min-h-[250px] w-full rounded-2xl bg-[url('/assets/abc.avif')] bg-cover bg-center bg-no-repeat">
+    <div className="aspect-[2/1] w-full rounded-2xl bg-[url('/assets/abc.avif')] bg-cover bg-center bg-no-repeat lg:aspect-[1/1.15]">
       <div className="flex h-full w-full items-end rounded-2xl bg-gradient-to-t from-gray-900">
-        <div className="lg::p-8 flex w-full flex-col gap-2 p-4">
-          <div className="flex items-center gap-4 text-gray-300 lg:gap-6">
+        <div className="flex w-full flex-col gap-2 p-8">
+          <div className="flex items-center gap-6 text-gray-300">
             <p>Mar 16,2020</p>
             <div className="h-1 w-1 rounded-full bg-gray-400"></div>
             <div className="flex items-center gap-3">
@@ -15,7 +15,7 @@ export default function BlogCard() {
               <p>Ashley</p>
             </div>
           </div>
-          <p className="text-lg font-semibold text-gray-200 lg:text-xl">
+          <p className="text-xl font-semibold text-gray-200">
             This is the blog that you can click to see
           </p>
         </div>
