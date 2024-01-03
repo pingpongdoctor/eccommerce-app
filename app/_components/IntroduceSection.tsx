e0fcbffe389ca9330fb09ec8a233945a3456a93e
Simon Tran <thanhnhantran1501@gmail.com>
2024-01-03 10:53:27 -0800

    refactor: redesign all components in the homepage to make them completely reponsive

--- app/_components/IntroduceSection.tsx
+++ app/_components/IntroduceSection.tsx
@@ -3,9 +3,9 @@ import Image from 'next/image';
 
 export default function IntroduceSection() {
   return (
-    <div className="z-1 relative bg-white pb-12 pt-12 lg:pb-16 lg:pt-16">
-      <div className="mb-8 flex gap-28">
-        <div className="flex w-[40%] flex-col gap-8 text-pretty">
+    <div className="z-1 relative bg-white p-4 pb-12 pt-12 md:p-8 lg:p-12 lg:pb-16 lg:pt-16 xl:mx-auto xl:max-w-7xl">
+      <div className="mb-8 block lg:flex lg:gap-16 xl:gap-28">
+        <div className="flex w-full flex-col gap-8 text-pretty lg:w-[40%]">
           <h1 className="mb-0">Our people</h1>
           <p className="text-xl">
             Quasi est quaerat. Sit molestiae et. Provident ad dolorem occaecati
@@ -21,17 +21,17 @@ export default function IntroduceSection() {
         <Image
           src="/assets/abc.avif"
           alt="hero-image-1"
-          className="h-auto grow rounded-2xl object-cover object-center"
+          className="hidden h-auto grow rounded-2xl object-cover object-center lg:block"
           width={300}
           height={300}
           priority={true}
         />
       </div>
-      <div className="flex items-start justify-between">
+      <div className="flex justify-between lg:items-start">
         <Image
           src="/assets/abc.avif"
           alt="hero-image-2"
-          className="h-auto w-[20%] self-end rounded-2xl"
+          className="hidden h-auto w-[20%] rounded-2xl lg:block"
           width={300}
           height={300}
           priority={true}
@@ -39,19 +39,29 @@ export default function IntroduceSection() {
         <Image
           src="/assets/abc.avif"
           alt="hero-image-3"
-          className="h-auto w-[45%] rounded-2xl"
+          className="h-auto w-[64%] rounded-2xl lg:w-[45%]"
           width={300}
           height={300}
           priority={true}
         />{' '}
-        <Image
-          src="/assets/abc.avif"
-          alt="hero-image-4"
-          className="h-auto w-[30%] rounded-2xl"
-          width={300}
-          height={300}
-          priority={true}
-        />
+        <div className="flex w-[31%] flex-col justify-between lg:block lg:w-[30%]">
+          <Image
+            src="/assets/abc.avif"
+            alt="hero-image-4"
+            className="h-auto w-full rounded-2xl"
+            width={300}
+            height={300}
+            priority={true}
+          />
+          <Image
+            src="/assets/abc.avif"
+            alt="hero-image-2"
+            className="h-auto w-full rounded-2xl lg:hidden"
+            width={300}
+            height={300}
+            priority={true}
+          />
+        </div>
       </div>
     </div>
   );
