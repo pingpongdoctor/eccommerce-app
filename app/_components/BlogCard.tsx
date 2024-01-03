dcbe9adc85237b68a02b44ff3616d981477b8818
Simon Tran <thanhnhantran1501@gmail.com>
2024-01-02 23:09:39 -0800

    refactor: turn center mode in carousel to false to fix bugs regarding designs

--- app/_components/BlogCard.tsx
+++ app/_components/BlogCard.tsx
@@ -4,18 +4,18 @@ import Avatar from './Avatar';
 
 export default function BlogCard() {
   return (
-    <div className="aspect-[1/1.15] w-[400px] rounded-2xl bg-[url('/assets/abc.avif')] bg-cover bg-center bg-no-repeat">
+    <div className="aspect-[1/1.15] min-h-[250px] w-full rounded-2xl bg-[url('/assets/abc.avif')] bg-cover bg-center bg-no-repeat">
       <div className="flex h-full w-full items-end rounded-2xl bg-gradient-to-t from-gray-900">
-        <div className="flex w-full flex-col gap-4 p-8">
-          <div className="flex items-center gap-4 text-gray-300">
+        <div className="lg::p-8 flex w-full flex-col gap-2 p-4">
+          <div className="flex items-center gap-4 text-gray-300 lg:gap-6">
             <p>Mar 16,2020</p>
-            <p className="h-1 w-1 rounded-full bg-gray-300"></p>
+            <div className="h-1 w-1 rounded-full bg-gray-400"></div>
             <div className="flex items-center gap-3">
               <Avatar avatarLink="/assets/abc.avif" avatarPriority={false} />
               <p>Ashley</p>
             </div>
           </div>
-          <p className="text-xl font-semibold text-gray-200">
+          <p className="text-lg font-semibold text-gray-200 lg:text-xl">
             This is the blog that you can click to see
           </p>
         </div>
--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,9 @@ import IntroduceSection from './_components/IntroduceSection';
 
 export default async function Home() {
   return (
-    <main className="p-4 md:p-8 lg:p-12">
-      <HeroSection />
-      <IntroduceSection />
+    <main className="p-4 *:mb-16 md:p-8 lg:p-12 xl:mx-auto xl:max-w-7xl">
+      {/* <HeroSection />
+      <IntroduceSection /> */}
       <BlogCards />
     </main>
   );
