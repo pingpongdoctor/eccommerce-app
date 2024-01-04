5d679320d6da0882699250e409f104d716b0d2e2
Simon Tran <thanhnhantran1501@gmail.com>
2024-01-04 12:12:46 -0800

    feat: change queries to sort products based on creating date and get the 13 newest featured products to show

--- app/_components/ProductCard.tsx
+++ app/_components/ProductCard.tsx
@@ -10,7 +10,7 @@ export default async function ProductCard({
   return (
     <div className="flex flex-col gap-2">
       <Image
-        className="object-fit aspect-[1/1.2] h-auto w-full rounded-lg object-cover object-center transition-all hover:opacity-90"
+        className="object-fit aspect-[1/1.2] h-auto w-full rounded-xl object-cover object-center transition-all hover:opacity-90"
         src={product.imgUrl}
         alt="product-image"
         width={300}
--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -1,6 +1,6 @@
 import { groq } from 'next-sanity';
 
-export const PRODUCTS_QUERY = groq`*[_type == "post" && defined(slug) && defined(category)]`;
-export const FEATURED_PRODUCTS_QUERY = groq`*[_type == "post" && defined(slug) && defined(category) && featured == $featured]`;
-export const PRODUCTS_QUERY_BASED_CATEGORY = groq`*[_type == "post" && category == $category]`;
+export const PRODUCTS_QUERY = groq`*[_type == "post" && defined(slug) && defined(category)] | order(_createdAt desc)`;
+export const FEATURED_PRODUCTS_QUERY = groq`*[_type == "post" && defined(slug) && defined(category) && featured == $featured] | order(_createdAt desc)[0...12]`;
+export const PRODUCTS_QUERY_BASED_CATEGORY = groq`*[_type == "post" && category == $category] | order(_createdAt desc)`;
 export const PRODUCT_QUERY = groq`*[_type == "post" && defined(slug) && defined(category) && slug.current == $slug][0]`;
