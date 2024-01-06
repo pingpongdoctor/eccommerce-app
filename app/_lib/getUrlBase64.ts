7178d6396b11b9ed0cd4fe04c2528f363e8b2026
Simon Tran <thanhnhantran1501@gmail.com>
2024-01-06 02:38:21 -0800

    refactor: refactor the getbase64url function and route handler

--- app/_lib/getUrlBase64.ts
+++ app/_lib/getUrlBase64.ts
@@ -1,24 +1,23 @@
-import React from 'react';
-// import { baseUrl } from '../utils/baseUrl';
+import { baseUrl } from '../utils/baseUrl';
 
-// // dynamic image need base64 url values for the blurDataUrl when using blur-mode placeholder
-// export const getUrlBase64 = async function name(
-//   imageUrl: string
-// ): Promise<string | undefined> {
-//   try {
-//     const res = await fetch(`${baseUrl}/api/get-base64?src=${imageUrl}`, {
-//       headers: { 'Content-Type': 'application/json' },
-//     });
+// dynamic image need base64 url values for the blurDataUrl when using blur-mode placeholder
+export const getUrlBase64 = async function name(
+  imageUrl: string
+): Promise<string | undefined> {
+  try {
+    const res = await fetch(`${baseUrl}/api/get-base64?src=${imageUrl}`, {
+      headers: { 'Content-Type': 'application/json' },
+    });
 
-//     if (!res.ok) {
-//       const errorData: { message: string } = await res.json();
-//       console.log('Error getting base64 url' + errorData.message);
-//       return undefined;
-//     }
-//     const data: { src: string } = await res.json();
-//     return data.src;
-//   } catch (e) {
-//     console.log('Error in getUrlBase64 function' + e);
-//     return undefined;
-//   }
-// };
+    if (!res.ok) {
+      const errorData: { message: string } = await res.json();
+      console.log('Error getting base64 url' + errorData.message);
+      return undefined;
+    }
+    const data: { src: string } = await res.json();
+    return data.src;
+  } catch (e) {
+    console.log('Error in getUrlBase64 function' + e);
+    return undefined;
+  }
+};
