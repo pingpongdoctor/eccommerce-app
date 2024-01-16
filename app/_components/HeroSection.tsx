3f78280de7a631a45b641ccae98f5c4226ba21eb
Simon Tran <thanhnhantran1501@gmail.com>
2024-01-16 12:18:45 -0800

    feat: add portable text to introduce section component

--- app/_components/HeroSection.tsx
+++ app/_components/HeroSection.tsx
@@ -1,15 +1,8 @@
 import React from 'react';
 import heroImage from '../../public/assets/pexels-scott-webb-403571.jpg';
 import Image from 'next/image';
-import { PlaceholderValue } from 'next/dist/shared/lib/get-img-props';
 
-export default function HeroSection({
-  heroImage,
-  blurHeroImage,
-}: {
-  heroImage: string;
-  blurHeroImage: string | undefined;
-}) {
+export default function HeroSection() {
   return (
     <div className="m-4 md:m-8 lg:m-12">
       <div className="relative xl:mx-auto xl:max-w-[74rem]">
@@ -19,9 +12,6 @@ export default function HeroSection({
           src={heroImage}
           alt="hero-image"
           placeholder="blur"
-          blurDataURL={blurHeroImage}
-          width={300}
-          height={300}
         />
         <h1 className="absolute left-0 top-0 ml-4 flex h-full w-[60%] items-center text-balance font-dancingScript text-4xl sm:text-5xl md:ml-8 md:text-6xl lg:ml-12 lg:text-7xl">
           Welcome to the Glowy Lab Family
--- app/_components/IntroduceSection.tsx
+++ app/_components/IntroduceSection.tsx
@@ -1,22 +1,20 @@
 import React from 'react';
 import Image from 'next/image';
 import testImage from '../../public/assets/abc.avif';
+import { PortableText } from '@portabletext/react';
 
-export default function IntroduceSection() {
+export default function IntroduceSection({
+  introContent,
+}: {
+  introContent: any;
+}) {
   return (
     <div className="bg-white px-4 pb-12 pt-12 md:px-8 lg:px-12 lg:pb-16 lg:pt-16 xl:mx-auto xl:max-w-7xl">
       <div className="mb-8 block lg:flex lg:gap-16 xl:gap-28">
-        <div className="flex w-full flex-col gap-8 text-pretty lg:w-[40%]">
-          <h1 className="mb-0">Our people</h1>
+        <div className="w-full text-pretty lg:w-[40%]">
+          <h1>Our people</h1>
           <p className="text-xl">
-            Quasi est quaerat. Sit molestiae et. Provident ad dolorem occaecati
-            eos iste. Soluta rerum quidem minus ut molestiae velit error quod.
-            Excepturi quidem expedita molestias quas.
-          </p>
-          <p className="text-xl">
-            Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
-            lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
-            fugiat. Quasi aperiam sit.
+            <PortableText value={introContent} />
           </p>
         </div>
         <Image
