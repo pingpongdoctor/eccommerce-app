c6ea9074d5ee24c472423e31a828f082cdcbfa12
Simon Tran <thanhnhantran1501@gmail.com>
2024-01-17 21:56:10 -0800

    fix: add products as a dependency for useEffect in client product card component

--- app/_components/ProductCard.tsx
+++ app/_components/ProductCard.tsx
@@ -2,26 +2,28 @@ import React from 'react';
 import { SanityDocument } from 'next-sanity';
 import Image from 'next/image';
 import { solidBlureDataUrl } from '../utils/utils';
+import ImageSkeleton from './ImageSkeleton';
 export default function ProductCard({
   product,
 }: {
   product: ProductWithImgUrl & SanityDocument;
 }) {
   return (
-    <div className="flex flex-col gap-2">
-      <Image
-        className="object-fit aspect-[1/1.2] h-auto w-full rounded-xl object-cover object-center transition-all hover:opacity-90"
-        src={product.imgUrl}
-        alt="product-image"
-        width={300}
-        height={300}
-        placeholder="blur"
-        blurDataURL={solidBlureDataUrl}
-      />
+    // <div className="flex flex-col gap-2">
+    //   <Image
+    //     className="object-fit aspect-[1/1.2] h-auto w-full rounded-xl object-cover object-center transition-all hover:opacity-90"
+    //     src={product.imgUrl}
+    //     alt="product-image"
+    //     width={300}
+    //     height={300}
+    //     placeholder="blur"
+    //     blurDataURL={solidBlureDataUrl}
+    //   />
 
-      <p>{product.title ? product.title : 'No Title'}</p>
+    //   <p>{product.title ? product.title : 'No Title'}</p>
 
-      <p className="text-lg text-gray-900">${product.price}</p>
-    </div>
+    //   <p className="text-lg text-gray-900">${product.price}</p>
+    // </div>
+    <ImageSkeleton />
   );
 }
