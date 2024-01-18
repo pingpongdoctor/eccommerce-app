ec2938a57a3cf628fee5b4768d3be485571cdef5
Simon Tran <thanhnhantran1501@gmail.com>
2024-01-17 20:43:54 -0800

    fix: use plaiceholder library to convert image url to base64 url

--- app/_components/ClientProductCards.tsx
+++ app/_components/ClientProductCards.tsx
@@ -3,6 +3,7 @@
 import { SanityDocument } from 'next-sanity';
 import Link from 'next/link';
 import ClientProductCard from './ClientProductCard';
+import { getUrlBase64 } from '../_lib/getUrlBase64';
 import { builder } from '../utils/imageBuilder';
 import { useEffect, useState } from 'react';
 
@@ -12,7 +13,7 @@ export default function ClientProductCards({
   products: (Product & SanityDocument)[];
 }) {
   const [productsWithImgUrl, setProductsWithImgUrl] = useState<
-    (Omit<ProductWithImgUrl, 'imgBase64Url'> & SanityDocument)[]
+    (ProductWithImgUrl & SanityDocument)[]
   >([]);
 
   // use promise all to handle all promises at the same time to avoid waterfalls in data fetching
@@ -20,14 +21,12 @@ export default function ClientProductCards({
     Promise.all(
       products.map(async (product: Product & SanityDocument) => {
         product.imgUrl = builder.image(product.images[0]).quality(80).url();
-        return product as Omit<ProductWithImgUrl, 'imgBase64Url'> &
-          SanityDocument;
+        product.imgBase64Url = await getUrlBase64(product.imgUrl);
+        return product as ProductWithImgUrl & SanityDocument;
       })
-    ).then(
-      (data: (Omit<ProductWithImgUrl, 'imgBase64Url'> & SanityDocument)[]) => {
-        setProductsWithImgUrl(data);
-      }
-    );
+    ).then((data: (ProductWithImgUrl & SanityDocument)[]) => {
+      setProductsWithImgUrl(data);
+    });
   }, []);
 
   if (productsWithImgUrl?.length > 0) {
@@ -35,9 +34,7 @@ export default function ClientProductCards({
       <div className="px-4 md:px-8 lg:px-12 xl:mx-auto xl:max-w-7xl">
         <div className="flex flex-col gap-4 sm:flex-row sm:flex-wrap sm:gap-8">
           {productsWithImgUrl.map(
-            (
-              product: Omit<ProductWithImgUrl, 'imgBase64Url'> & SanityDocument
-            ) => (
+            (product: ProductWithImgUrl & SanityDocument) => (
               <Link
                 className="inline-block w-full sm:w-[calc((100%-2rem)/2)] lg:w-[calc((100%-4rem)/3)] xl:w-[calc((100%-6rem)/4)]"
                 key={product._id}
--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,5 @@
+import withPlaiceholder from '@plaiceholder/next';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
@@ -14,4 +16,4 @@ const nextConfig = {
   // ...other config settings
 };
 
-export default nextConfig;
+export default withPlaiceholder(nextConfig);
