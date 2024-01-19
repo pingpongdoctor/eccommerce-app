0c946388d6225d13ee1a0f13ae02318853a521c7
Simon Tran <thanhnhantran1501@gmail.com>
2024-01-19 12:06:15 -0800

    fix: fix the product cards component to allow it consuming the isCustomerAlsoBuyData

--- app/product/[slug]/page.tsx
+++ app/product/[slug]/page.tsx
@@ -1,12 +1,18 @@
 import { SanityDocument, QueryParams } from 'next-sanity';
 import ProductDetail from '@/app/_components/ProductDetail';
 import { loadQuery } from '@/sanity/lib/store';
-import { PRODUCT_QUERY, PRODUCTS_QUERY } from '@/sanity/lib/queries';
+import {
+  PRODUCT_QUERY,
+  PRODUCTS_QUERY,
+  PRODUCTS_QUERY_CUSTOMER_ALSO_BUY,
+} from '@/sanity/lib/queries';
 import { draftMode } from 'next/headers';
 import ProductDetailPreview from '@/app/_components/ProductDetailPreview';
 import { client } from '@/sanity/lib/client';
 import { Metadata } from 'next';
 import { notFound } from 'next/navigation';
+import ProductCards from '@/app/_components/ProductCards';
+import ProductCardsPreview from '@/app/_components/ProductCardsPreview';
 
 export async function generateMetadata({
   params,
@@ -60,13 +66,15 @@ export default async function DetailedProduct({
     }
   );
 
-  if (!initialData.data) {
+  if (!initialData?.data) {
     notFound();
   }
 
-  const customerAlsoBuyInitialData = await loadQuery<Product & SanityDocument>(
-    PRODUCT_QUERY,
-    params,
+  const customerAlsoBuyInitialData = await loadQuery<
+    (Product & SanityDocument)[]
+  >(
+    PRODUCTS_QUERY_CUSTOMER_ALSO_BUY,
+    { category: initialData.data.category },
     {
       perspective: draftMode().isEnabled ? 'previewDrafts' : 'published',
       next: { tags: ['post'], revalidate: 3600 },
@@ -79,6 +87,12 @@ export default async function DetailedProduct({
         <ProductDetailPreview initial={initialData} params={params} />
       ) : (
         <ProductDetail product={initialData.data} />
+      )}
+
+      {draftMode().isEnabled ? (
+        <ProductCardsPreview initial={customerAlsoBuyInitialData} />
+      ) : (
+        <ProductCards products={customerAlsoBuyInitialData.data} />
       )}
     </main>
   );
