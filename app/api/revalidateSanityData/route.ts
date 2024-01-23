534fb0431b416336ffcdd80c7e8fb9ea789dff81
Simon Tran <thanhnhantran1501@gmail.com>
2024-01-22 16:12:38 -0800

    feat: create a new api endpoint to create new product using data that is posted from Sanity leveraging Sanity webhooks

--- app/api/revalidateSanityData/route.ts
+++ app/api/revalidateSanityData/route.ts
@@ -5,8 +5,7 @@ import { parseBody } from 'next-sanity/webhook';
 export async function POST(req: NextRequest) {
   const { body, isValidSignature } = await parseBody<{
     _type: string;
-    slug?: string | undefined;
-  }>(req, process.env.NEXT_PUBLIC_SANITY_HOOK_SECRET);
+  } | null>(req, process.env.NEXT_PUBLIC_SANITY_HOOK_SECRET);
 
   if (!isValidSignature) {
     return new Response('Invalid Signature', { status: 401 });
