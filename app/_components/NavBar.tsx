a310533506cddc9f90c633cfadea0d06f2b234c7
Simon Tran <thanhnhantran1501@gmail.com>
2024-01-24 11:43:27 -0800

    feat: develop a simple menu and add it to the nav bar

--- app/_components/NavBar.tsx
+++ app/_components/NavBar.tsx
@@ -6,18 +6,41 @@ import Image from 'next/image';
 import Link from 'next/link';
 import glowlyLab from '../../public/assets/glowy-lab.png';
 import { useUser } from '@auth0/nextjs-auth0/client';
-import Avatar from './Avatar';
 import { ArrowLongRightIcon } from '@heroicons/react/16/solid';
+import { getUser } from '../_lib/getUser';
+import { useState, useEffect } from 'react';
+import { User } from '@prisma/client';
+import SimpleMenuComponent from './SimpleMenuComponent';
+import { ThreeDots } from 'react-loader-spinner';
 
 export default function Navbar() {
-  const { user } = useUser();
+  const [userProfile, setUserProfile] = useState<Omit<User, 'auth0Id'> | null>(
+    null
+  );
+
+  const { user, isLoading } = useUser();
+
+  useEffect(() => {
+    if (user) {
+      getUser()
+        .then((userData: Omit<User, 'auth0Id'> | undefined) => {
+          console.log(userData);
+          if (userData) {
+            setUserProfile(userData);
+          }
+        })
+        .catch((e: any) => {
+          console.log('error fetching user data' + ' ' + e);
+        });
+    }
+  }, [user]);
 
   return (
     <div className="mx-auto mb-8 flex max-w-7xl flex-col gap-2 p-4 text-sm text-gray-900 md:block md:p-8 lg:mb-12 lg:p-12">
       <nav className="flex h-8 items-center justify-between overflow-visible bg-white">
         <Link href="/" className="-m-1.5 p-1.5">
           <Image
-            className="h-24 w-auto md:h-28"
+            className="h-20 w-auto md:h-28"
             src={glowlyLab}
             alt="logo"
             priority
@@ -34,38 +57,62 @@ export default function Navbar() {
           <Link href="/contact">Contact</Link>
         </div>
 
-        {!user && (
-          <Link
-            href="/api/auth/login"
-            className="group relative h-[20px] w-[88px] font-semibold"
-          >
-            <span className="group absolute left-0 top-0 z-[1] flex h-full w-full items-center justify-center gap-2 group-hover:text-white">
-              <span>Log in</span>
-              <span className="group-hover:hidden">&rarr;</span>
-              <ArrowLongRightIcon className="hidden h-5 group-hover:block" />
-            </span>
+        <div
+          className={`flex w-auto justify-end transition-all md:w-[144px] md:items-center ${
+            isLoading ? 'lg:justify-end' : 'lg:justify-between'
+          }`}
+        >
+          {!user && !isLoading && (
+            <Link
+              href="/api/auth/login"
+              className="group relative ml-auto h-[25px] w-[88px] font-semibold"
+            >
+              <span className="group absolute left-0 top-0 z-[1] flex h-full w-full items-center justify-center gap-2 group-hover:text-white">
+                <span>Log in</span>
+                <span className="group-hover:hidden">&rarr;</span>
+                <ArrowLongRightIcon className="hidden h-5 group-hover:block" />
+              </span>
 
-            <span className="absolute left-0 top-0 z-0 h-full w-0 rounded-lg bg-gray-900 transition-all group-hover:w-full"></span>
-          </Link>
-        )}
-        {user && (
-          <Link
-            href="/api/auth/logout"
-            className="group relative h-[25px] w-[88px] font-semibold"
-          >
-            <span className="group absolute left-0 top-0 z-[1] flex h-full w-full items-center justify-center gap-2 group-hover:text-white">
-              <span>Log out</span>
-              <span className="group-hover:hidden">&rarr;</span>
-              <ArrowLongRightIcon className="hidden h-5 group-hover:block" />
-            </span>
+              <span className="absolute left-0 top-0 z-0 h-full w-0 rounded-lg bg-gray-900 transition-all group-hover:w-full"></span>
+            </Link>
+          )}
 
-            <span className="absolute left-0 top-0 z-0 h-full w-0 rounded-lg bg-gray-900 transition-all group-hover:w-full"></span>
-          </Link>
-        )}
+          {user && userProfile && (
+            <SimpleMenuComponent
+              avatarSrc={userProfile.imgUrl}
+              username={userProfile.name}
+            />
+          )}
+
+          {user && userProfile && !isLoading && (
+            <Link
+              href="/api/auth/logout"
+              className="group relative hidden h-[25px] w-[88px] font-semibold lg:block"
+            >
+              <span className="group absolute left-0 top-0 z-[1] flex h-full w-full items-center justify-center gap-2 group-hover:text-white">
+                <span>Log out</span>
+                <span className="group-hover:hidden">&rarr;</span>
+                <ArrowLongRightIcon className="hidden h-5 group-hover:block" />
+              </span>
+
+              <span className="absolute left-0 top-0 z-0 h-full w-0 rounded-lg bg-gray-900 transition-all group-hover:w-full"></span>
+            </Link>
+          )}
+
+          {isLoading && (
+            <ThreeDots
+              visible={true}
+              height="50"
+              width="50"
+              color="black"
+              radius="9"
+            />
+          )}
+        </div>
       </nav>
 
       {/* mobile navigation links */}
-      <div className="mx-auto flex gap-x-4 md:hidden [&>a]:font-semibold">
+      <div className="mx-auto flex items-center gap-6 md:hidden [&>a]:font-semibold">
         <DropdownMenu dropdownItemInforArr={dropdownItemInforArr} />
         <Link href="#">Features</Link>
         <Link href="#">Marketplace</Link>
---
+++ app/_lib/getUser.ts
@@ -0,0 +1,24 @@
+import { User } from '@prisma/client';
+import { baseUrl } from '../utils/baseUrl';
+
+export async function getUser(): Promise<Omit<User, 'auth0Id'> | undefined> {
+  try {
+    const res = await fetch(`${baseUrl}/api/user`, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    const data = await res.json();
+
+    if (!res.ok) {
+      console.log(data.message);
+      return undefined;
+    }
+
+    return data.data;
+  } catch (e: any) {
+    console.log('Error fetching user data' + ' ' + e);
+    return undefined;
+  }
+}
