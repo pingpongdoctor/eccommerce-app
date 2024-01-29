390d3674fab40e0586242e4060c691fe07dbd09b
Simon Tran <thanhnhantran1501@gmail.com>
2024-01-29 14:09:45 -0800

    refactor: refactor the notfound page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,9 +1,9 @@
-import React from "react";
-import Link from "next/link";
+import React from 'react';
+import Link from 'next/link';
 
 export default function NotFound() {
   return (
-    <div>
+    <div className="min-h-[300px] px-4 md:px-8 lg:px-12 xl:mx-auto xl:max-w-7xl">
       <h2>Not Found</h2>
       <p>Could not find requested resource</p>
       <Link href="/">Return Home</Link>
