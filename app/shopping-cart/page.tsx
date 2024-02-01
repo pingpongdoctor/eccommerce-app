02af4ccf3a767873ba05dce141ecb148cf517eb6
Simon Tran <thanhnhantran1501@gmail.com>
2024-02-01 15:24:50 -0800

    feat: develop a skeleton component for the order summary component

--- app/shopping-cart/page.tsx
+++ app/shopping-cart/page.tsx
@@ -9,6 +9,7 @@ import { client } from '@/sanity/lib/client';
 import { useContext } from 'react';
 import { globalStatesContext } from '../_components/GlobalStatesContext';
 import ShoppingCartItemSkeleton from '../_components/ShoppingCartItemSkeleton';
+import OrderSummarySkeleton from '../_components/OrderSummarySkeleton';
 
 export default function ShoppingCart() {
   const { userProfile, changeProductsInCart, setChangeProductsInCart } =
@@ -70,22 +71,34 @@ export default function ShoppingCart() {
       <h2 className="mx-auto max-w-7xl px-4 md:px-8 lg:px-12">Shopping Cart</h2>
 
       <div className="flex flex-col px-4 md:px-8 lg:flex-row lg:justify-between lg:px-12 xl:mx-auto xl:max-w-7xl">
-        {isFetchingSanityProducts && (
+        {/* {isFetchingSanityProducts && (
           <div className="mb-8 *:mb-8 md:w-[35%] lg:mb-12">
             <ShoppingCartItemSkeleton />
             <ShoppingCartItemSkeleton />
           </div>
-        )}
+        )} */}
 
-        {sanityProductsInCart.length > 0 && !isFetchingSanityProducts && (
+        {/* {sanityProductsInCart.length > 0 && !isFetchingSanityProducts && (
           <ShoppingCartList
             products={productsInCart}
             sanityProducts={sanityProductsInCart}
             shoppingCartListClassname="lg:w-[50%]"
           />
-        )}
+        )} */}
 
-        <OrderSummaryComponent orderSummaryComponentClassname="lg:w-[40%]" />
+        {/* <ShoppingCartList
+          products={productsInCart}
+          sanityProducts={sanityProductsInCart}
+          shoppingCartListClassname="lg:w-[50%]"
+        /> */}
+
+        <div className="mb-8 w-full *:mb-8 md:w-[50%] lg:mb-12">
+          <ShoppingCartItemSkeleton />
+          <ShoppingCartItemSkeleton />
+        </div>
+
+        {/* <OrderSummaryComponent orderSummaryComponentClassname="lg:w-[40%]" /> */}
+        <OrderSummarySkeleton />
       </div>
 
       {sanityProductsInCart.length == 0 && !isFetchingSanityProducts && (
