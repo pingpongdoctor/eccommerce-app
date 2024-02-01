f82a171f356fb1318c7382c779a6ff9ed7dce631
Simon Tran <thanhnhantran1501@gmail.com>
2024-01-31 16:20:06 -0800

    fix: convert shopping-cart page to CSR page

--- app/shopping-cart/page.tsx
+++ app/shopping-cart/page.tsx
@@ -1,40 +1,86 @@
-import { loadQuery } from '@/sanity/lib/store';
-import { withPageAuthRequired } from '@auth0/nextjs-auth0';
+'use client';
 import { getProductsInCartFromServer } from '../_lib/getProductsInCartFromServer';
 import { SanityDocument } from 'next-sanity';
 import { draftMode } from 'next/headers';
-import { PRODUCTS_QUERY_BASED_SLUGS } from '@/sanity/lib/queries';
+import { PRODUCTS_QUERY_BY_SLUGS } from '@/sanity/lib/queries';
 import { QueryResponseInitial } from '@sanity/react-loader';
 import ShoppingCartList from '../_components/ShoppingCartList';
+import OrderSummaryComponent from '../_components/OrderSummaryComponent';
+import { useEffect, useState } from 'react';
+import { useUser } from '@auth0/nextjs-auth0/client';
+import { getUserProfileFromClientSide } from '../_lib/getUserProfileFromClientSide';
+import { User } from '@prisma/client';
+import { client } from '@/sanity/lib/client';
 
-export default withPageAuthRequired(async function ShoppingCart() {
-  //get products in shopping cart of the current user
-  const products: ProductInShoppingCart[] | undefined =
-    await getProductsInCartFromServer();
+export default function ShoppingCart() {
+  const { user } = useUser();
+  const [productsInCart, setProductsInCart] = useState<ProductInShoppingCart[]>(
+    []
+  );
+  const [sanityProductsInCart, setSanityProductsInCart] = useState<
+    (SanityProduct & SanityDocument)[]
+  >([]);
+  const [userProfile, setUserProfile] = useState<Omit<User, 'auth0Id'> | null>(
+    null
+  );
 
-  //get product sanity documents
-  const productSlugs: string[] = products
-    ? products.map((product: ProductInShoppingCart) => product.productSlug)
-    : [];
+  useEffect(() => {
+    //get user profile
+    if (user) {
+      getUserProfileFromClientSide().then(
+        (userData: Omit<User, 'auth0Id'> | undefined) => {
+          if (userData) {
+            setUserProfile(userData);
+          }
+        }
+      );
+    }
+  }, [user]);
 
-  const initialData: QueryResponseInitial<(SanityProduct & SanityDocument)[]> =
-    await loadQuery<(SanityProduct & SanityDocument)[]>(
-      PRODUCTS_QUERY_BASED_SLUGS,
-      { slugArr: productSlugs },
-      {
-        perspective: draftMode().isEnabled ? 'previewDrafts' : 'published',
-      }
-    );
+  useEffect(() => {
+    if (userProfile) {
+      //get products in shopping cart of the current user
+      getProductsInCartFromServer()
+        .then((productsInCart: ProductInShoppingCart[] | undefined) => {
+          if (productsInCart) {
+            setProductsInCart(productsInCart);
+          }
+        })
+        .catch((e: any) => {
+          console.log(e.message);
+        });
+    }
+  }, [userProfile]);
 
-  const sanityProducts: (SanityProduct & SanityDocument)[] = initialData.data;
+  useEffect(() => {
+    if (productsInCart.length > 0) {
+      //get product sanity documents
+      const productSlugs: string[] = productsInCart
+        ? productsInCart.map(
+            (product: ProductInShoppingCart) => product.productSlug
+          )
+        : [];
+
+      client
+        .fetch<(SanityProduct & SanityDocument)[]>(PRODUCTS_QUERY_BY_SLUGS, {
+          slugArr: productSlugs,
+        })
+        .then((products: (SanityProduct & SanityDocument)[]) => {
+          setSanityProductsInCart(products);
+        });
+    }
+  }, [productsInCart]);
 
   return (
-    <div>
-      <h2>Shopping Cart</h2>
+    <main>
+      <h2 className="mx-auto max-w-7xl px-4 md:px-8 lg:px-12">Shopping Cart</h2>
+
       <ShoppingCartList
-        products={products || []}
-        sanityProducts={sanityProducts}
+        products={productsInCart}
+        sanityProducts={sanityProductsInCart}
       />
-    </div>
+
+      <OrderSummaryComponent />
+    </main>
   );
-});
+}
