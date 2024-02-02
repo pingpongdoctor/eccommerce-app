16bbf81b6f2714587895683b968dde4b7ff260cb
Simon Tran <thanhnhantran1501@gmail.com>
2024-02-01 16:29:10 -0800

    feat: create a function to add product quantity

--- app/_components/ShoppingCartList.tsx
+++ app/_components/ShoppingCartList.tsx
@@ -2,45 +2,17 @@
 
 import { SanityDocument } from 'next-sanity';
 import ShoppingCartItem from './ShoppingCartItem';
-import { addProductImgUrls } from '../_lib/addProductImgUrls';
-import { useState, useEffect } from 'react';
 
 interface Props {
-  sanityProducts: (SanityProduct & SanityDocument)[];
-  products: ProductInShoppingCart[];
+  productsWithImgUrlAndQuantity: (ProductWithImgUrl &
+    SanityDocument & { productQuantity: number })[];
   shoppingCartListClassname?: string;
 }
 
 export default function ShoppingCartList({
-  sanityProducts,
-  products,
+  productsWithImgUrlAndQuantity,
   shoppingCartListClassname,
 }: Props) {
-  const [productsWithImgUrlAndQuantity, setProductsWithImgUrlAndQuantity] =
-    useState<
-      (ProductWithImgUrl & SanityDocument & { productQuantity: number })[]
-    >([]);
-
-  // set img url and product quantity to sanity documents
-  useEffect(() => {
-    addProductImgUrls(sanityProducts).then(
-      (productsWithImgUrl: (ProductWithImgUrl & SanityDocument)[]) => {
-        const productsWithImgAndQuantity = [...productsWithImgUrl].map(
-          (sanityProduct: ProductWithImgUrl & SanityDocument) => {
-            for (let i = 0; i < products.length; i++) {
-              if (sanityProduct.slug.current === products[i].productSlug) {
-                sanityProduct.productQuantity = products[i].productQuantity;
-              }
-            }
-            return sanityProduct as ProductWithImgUrl &
-              SanityDocument & { productQuantity: number };
-          }
-        );
-        setProductsWithImgUrlAndQuantity(productsWithImgAndQuantity);
-      }
-    );
-  }, [products, sanityProducts]);
-
   return (
     <div className={`mb-8 *:mb-8 lg:mb-12 ${shoppingCartListClassname}`}>
       {productsWithImgUrlAndQuantity.map(
---
+++ app/_lib/addProductQuantity.ts
@@ -0,0 +1,19 @@
+import { SanityDocument } from 'next-sanity';
+export function addProductQuantity(
+  productsWithImgUrl: (ProductWithImgUrl & SanityDocument)[],
+  productsInCart: ProductInShoppingCart[]
+): (ProductWithImgUrl & SanityDocument & { productQuantity: number })[] {
+  const productsWithImgAndQuantity = [...productsWithImgUrl].map(
+    (sanityProduct: ProductWithImgUrl & SanityDocument) => {
+      for (let i = 0; i < productsInCart.length; i++) {
+        if (sanityProduct.slug.current === productsInCart[i].productSlug) {
+          sanityProduct.productQuantity = productsInCart[i].productQuantity;
+        }
+      }
+      return sanityProduct as ProductWithImgUrl &
+        SanityDocument & { productQuantity: number };
+    }
+  );
+
+  return productsWithImgAndQuantity;
+}
