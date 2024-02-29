b30543e3973095e2ae42ceab8f6b4aa4d67c4772
Simon Tran <thanhnhantran1501@gmail.com>
2024-02-29 15:31:08 +0700

    fix: omit user id field when returning user profile

--- app/_components/GlobalStatesContext.tsx
+++ app/_components/GlobalStatesContext.tsx
@@ -19,9 +19,10 @@ export default function GlobalStatesContext({
     needToRevalidateDataForShoppingCartPage,
     setNeedToRevalidateDataForShoppingCartPage,
   ] = useState<boolean>(false);
-  const [userProfile, setUserProfile] = useState<Omit<User, 'auth0Id'> | null>(
-    null
-  );
+  const [userProfile, setUserProfile] = useState<Omit<
+    User,
+    'auth0Id' | 'id'
+  > | null>(null);
   const [productsInCart, setProductsInCart] = useState<ProductInShoppingCart[]>(
     []
   );
@@ -30,8 +31,9 @@ export default function GlobalStatesContext({
     //set user profile state
     if (user && !isLoading) {
       getUserProfileFromClientSide().then(
-        (userData: Omit<User, 'auth0Id'> | undefined) => {
+        (userData: Omit<User, 'auth0Id' | 'id'> | undefined) => {
           if (userData) {
+            console.log(userData);
             setUserProfile(userData);
           } else {
             setUserProfile(null);
