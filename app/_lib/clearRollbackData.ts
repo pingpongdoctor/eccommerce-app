cded4e4925519623b26d1541f80ca0ee8894b705
Simon Tran <thanhnhantran1501@gmail.com>
2024-03-02 14:37:48 +0700

    fix: fix clear-rollback-data endpoint to clear rollback data with rollback data key instead of with user id

--- app/_lib/clearRollbackData.ts
+++ app/_lib/clearRollbackData.ts
@@ -1,12 +1,16 @@
 import { baseUrl } from '../utils/baseUrl';
 
 //return true if rollback data is cleared
-export async function clearRollbackData(): Promise<boolean> {
+export async function clearRollbackData(
+  rollbackDataKey: string
+): Promise<boolean> {
   try {
     const res = await fetch(`${baseUrl}/api/clear-rollback-data`, {
       headers: {
         'Content-Type': 'application/json',
       },
+      method: 'POST',
+      body: JSON.stringify({ rollbackDataKey }),
     });
 
     const data = await res.json();
