19d1e15034506f2ddc44f3449f9f880700b79d74
Simon Tran <thanhnhantran1501@gmail.com>
2024-04-01 17:01:48 -0700

    feat: create BlogCardsComponent

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -14,6 +14,7 @@ import BlogPreviewComponent from '@/app/_components/BlogPreviewComponent';
 import BlogCard from '@/app/_components/BlogCard';
 import { addDetailedAuthorDataToBlogs } from '@/app/_lib/addDetailedAuthorToBlogs';
 import Link from 'next/link';
+import BlogCardsComponent from '@/app/_components/BlogCardsComponent';
 
 export async function generateMetadata({
   params,
@@ -113,27 +114,11 @@ export default async function DetailedBlog({
         )}
 
         <div className="flex flex-wrap justify-between gap-y-4 px-4 sm:gap-y-8 md:px-8 lg:px-12 xl:mx-auto xl:max-w-7xl">
-          {blogsCustomerAlsoReadWithDetailedAuthorData.length > 0 &&
-            blogsCustomerAlsoReadWithDetailedAuthorData.map(
-              (
-                blog: SanityBlog &
-                  SanityDocument & {
-                    authorData: SanityAuthor & SanityDocument;
-                  } & { imageUrl: string }
-              ) => (
-                <Link
-                  className="block w-full md:w-[calc((100%-2rem)/2)] lg:w-[calc((100%-4rem)/3)] xl:w-[calc((100%-6rem)/4)]"
-                  key={blog._id}
-                  href={`/blog/${blog.slug.current}`}
-                >
-                  {' '}
-                  <BlogCard
-                    blog={blog}
-                    blogCardClassname="w-full sm:w-full md:w-full lg:w-full xl:w-full md:aspect-[1/1.15]"
-                  />
-                </Link>
-              )
-            )}
+          {blogsCustomerAlsoReadWithDetailedAuthorData.length > 0 && (
+            <BlogCardsComponent
+              blogs={blogsCustomerAlsoReadWithDetailedAuthorData}
+            />
+          )}
         </div>
       </div>
     </main>
