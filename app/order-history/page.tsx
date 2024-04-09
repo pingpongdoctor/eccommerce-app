2c6c666fc6804b0ecd9d50899c4ce4e9bfb2f138
Simon Tran <thanhnhantran1501@gmail.com>
2024-04-08 21:05:31 -0700

    fix: fix get-orders endpoint

--- app/order-history/page.tsx
+++ app/order-history/page.tsx
@@ -1,5 +1,21 @@
-import React from 'react';
+import { redirect } from 'next/navigation';
+import { getAllOrdersOnServerSide } from '../_lib/getAllOrdersOnServerSide';
+import { getUserProfileFromServer } from '../_lib/getUserProfileFromServer';
+import { notify } from '../_components/ReactToastifyProvider';
 
-export default function OrderHistoryPage() {
-  return <div>page</div>;
+export default async function OrderHistoryPage() {
+  const userData = await getUserProfileFromServer();
+
+  if (!userData) {
+    notify(
+      'info',
+      'can not access the order history page unless you log in',
+      'not-authenticated'
+    );
+    redirect('/');
+  }
+
+  const data = await getAllOrdersOnServerSide();
+
+  return <div></div>;
 }
