18d05551734cb82fad298efa443c3cc53bab52c5
Simon Tran <thanhnhantran1501@gmail.com>
2024-04-14 23:02:28 -0700

    feat: create generateOrderInforObject function

--- app/_components/HistoryOrderItemComponent.tsx
+++ app/_components/HistoryOrderItemComponent.tsx
@@ -1,34 +1,14 @@
-import Image from 'next/image';
 import { CheckCircleIcon } from '@heroicons/react/16/solid';
 import { formatDateToWords } from '../_lib/formatDateToWords';
 import HistoryOrderProduct from './HistoryOrderProduct';
+import generateOrderInforObject from '../_lib/generateOrderInforObject';
 
 interface Props {
   order: Order;
 }
 
 export default function HistoryOrderItemComponent({ order }: Props) {
-  const orderInfor = [
-    { id: 1, field: 'Order number', value: order.transactionNumber },
-    {
-      id: 2,
-      field: 'Date Placed',
-      value: formatDateToWords(order.placedDate),
-    },
-    { id: 3, field: 'Subtotal', value: order.subtotal },
-    {
-      id: 4,
-      field: 'Total',
-      value:
-        Number(order.subtotal) + Number(order.tax) + Number(order.shipping),
-    },
-    {
-      id: 5,
-      field: 'Total (include shipping and tax)',
-      value:
-        Number(order.subtotal) + Number(order.tax) + Number(order.shipping),
-    },
-  ];
+  const orderInfor = generateOrderInforObject(order);
   return (
     <li className="rounded-md border border-gray-200">
       <div className="p-4 pb-0 md:p-8 md:pb-0 lg:p-12 lg:pb-0">
---
+++ app/_lib/generateOrderInforObject.tsx
@@ -0,0 +1,25 @@
+import { formatDateToWords } from './formatDateToWords';
+
+export default function generateOrderInforObject(order: Order) {
+  return [
+    { id: 1, field: 'Order number', value: order.transactionNumber },
+    {
+      id: 2,
+      field: 'Date Placed',
+      value: formatDateToWords(order.placedDate),
+    },
+    { id: 3, field: 'Subtotal', value: order.subtotal },
+    {
+      id: 4,
+      field: 'Total',
+      value:
+        Number(order.subtotal) + Number(order.tax) + Number(order.shipping),
+    },
+    {
+      id: 5,
+      field: 'Total (include shipping and tax)',
+      value:
+        Number(order.subtotal) + Number(order.tax) + Number(order.shipping),
+    },
+  ];
+}
