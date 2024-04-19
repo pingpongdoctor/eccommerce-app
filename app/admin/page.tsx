52b990421514b4e247fe166955cc522f06968a25
Simon Tran <thanhnhantran1501@gmail.com>
2024-04-18 17:10:26 -0700

    feat: create middleware to check user role for authorization

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -2,16 +2,5 @@ import { getSession } from '@auth0/nextjs-auth0';
 import { redirect } from 'next/navigation';
 
 export default async function AdminPage() {
-  const session = await getSession();
-
-  const isAdmin =
-    session?.user[process.env.AUTH0_CUSTOM_ROLE_CLAIM as string].include(
-      'admin'
-    );
-
-  if (!isAdmin) {
-    redirect('/');
-  }
-
-  return <div></div>;
+  return <div>Admin data</div>;
 }
---
+++ middleware.ts
@@ -0,0 +1,24 @@
+import {
+  withMiddlewareAuthRequired,
+  getSession,
+} from '@auth0/nextjs-auth0/edge';
+import { NextRequest, NextResponse } from 'next/server';
+
+export default withMiddlewareAuthRequired(async function middleware(
+  req: NextRequest
+) {
+  const session = await getSession();
+
+  const isAdmin =
+    session?.user[process.env.AUTH0_CUSTOM_ROLE_CLAIM as string].includes(
+      'admin'
+    );
+
+  if (!isAdmin) {
+    return NextResponse.redirect(new URL('/', req.url));
+  }
+});
+
+export const config = {
+  matcher: '/admin',
+};
