ef093b01558972a56639c761595abe7df227a778
Simon Tran <thanhnhantran1501@gmail.com>
2024-04-20 21:46:49 -0700

    feat: design the black theme for admin page

--- app/_components/FooterComponent.tsx
+++ app/_components/FooterComponent.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import Image from 'next/image';
 import glowyLab from '../../public/assets/glowy-lab.png';
 import Link from 'next/link';
@@ -7,45 +9,60 @@ import {
   IconBrandYoutube,
 } from '@tabler/icons-react';
 import { footerInfor } from '../utils/utils';
+import { usePathname } from 'next/navigation';
 
 export default function FooterComponent() {
-  return (
-    <div className="flex min-h-[70vh] flex-col p-4 text-sm text-gray-600 md:p-8 lg:flex-row lg:items-center lg:gap-14 lg:p-12 xl:mx-auto xl:max-w-7xl">
-      <div className="mb-8 md:mb-10 lg:max-w-[400px]">
-        <Link href="/" className="inline-block w-32">
-          <Image
-            src={glowyLab}
-            alt="logo"
-            className="relative -left-[1.7rem] h-32 w-auto md:mb-4"
-            placeholder="blur"
-          />
-        </Link>
-        <p className="mb-4 md:mb-8">
-          Making the world a better place through constructing elegant
-          hierarchies.
-        </p>
-        <div className=" relative -left-1 flex gap-8 *:h-5 *:w-auto hover:*:text-indigo-600 md:gap-8 md:*:h-7">
-          <IconBrandFacebook />
-          <IconBrandInstagram />
-          <IconBrandYoutube />
-        </div>
-      </div>
+  const pathname = usePathname();
 
-      <ul className="flex list-none flex-wrap justify-between gap-4 gap-y-10 md:flex-row md:gap-8 lg:grow lg:gap-10 xl:flex-nowrap">
-        {footerInfor.map((infor) => (
-          <li
-            key={infor.id}
-            className="w-[45%] *:mb-4 first:*:mb-6 last:*:mb-0 md:w-auto md:flex-1"
-          >
-            <p className="font-semibold text-gray-900">{infor.heading}</p>
-            {infor.detail.map((ele) => (
-              <Link className="block" href={ele.href} key={ele.id}>
-                {ele.name}
-              </Link>
-            ))}
-          </li>
-        ))}
-      </ul>
+  return (
+    <div className={`${pathname === '/admin' ? 'bg-gray-900' : ''}`}>
+      <div
+        className={`flex min-h-[70vh] flex-col p-4 text-sm text-gray-600 md:p-8 lg:flex-row lg:items-center lg:gap-14 lg:p-12 xl:mx-auto xl:max-w-7xl ${
+          pathname === '/admin' ? 'text-white' : ''
+        }`}
+      >
+        <div className="mb-8 md:mb-10 lg:max-w-[400px]">
+          <Link href="/" className="inline-block w-32">
+            {pathname === '/admin' ? (
+              <p className="mb-4 w-32 font-dancingScript text-4xl font-bold">
+                Glowy
+              </p>
+            ) : (
+              <Image
+                src={glowyLab}
+                alt="logo"
+                className="relative -left-[1.7rem] h-32 w-auto md:mb-4"
+                placeholder="blur"
+              />
+            )}
+          </Link>
+          <p className="mb-4 md:mb-8">
+            Making the world a better place through constructing elegant
+            hierarchies.
+          </p>
+          <div className=" relative -left-1 flex gap-8 *:h-5 *:w-auto hover:*:text-indigo-600 md:gap-8 md:*:h-7">
+            <IconBrandFacebook />
+            <IconBrandInstagram />
+            <IconBrandYoutube />
+          </div>
+        </div>
+
+        <ul className="flex list-none flex-wrap justify-between gap-4 gap-y-10 md:flex-row md:gap-8 lg:grow lg:gap-10 xl:flex-nowrap">
+          {footerInfor.map((infor) => (
+            <li
+              key={infor.id}
+              className="w-[45%] *:mb-4 first:*:mb-6 last:*:mb-0 md:w-auto md:flex-1"
+            >
+              <p className="font-semibold text-gray-900">{infor.heading}</p>
+              {infor.detail.map((ele) => (
+                <Link className="block" href={ele.href} key={ele.id}>
+                  {ele.name}
+                </Link>
+              ))}
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
--- app/_components/NavBar.tsx
+++ app/_components/NavBar.tsx
@@ -11,150 +11,165 @@ import { ThreeDots } from 'react-loader-spinner';
 import { globalStatesContext } from './GlobalStatesContext';
 import { calculateTotalProducts } from '../_lib/calculateTotalProducts';
 import { useContext } from 'react';
+import { navigationLinksInfor } from '../utils/utils';
+import { usePathname } from 'next/navigation';
 
 export default function Navbar() {
   const { userProfile, isLoading, productsInCart } =
     useContext(globalStatesContext);
 
-  const linkClassname =
-    'transition-all hover:border-b-2 hover:border-gray-900 hover:pb-[2px] hover:pt-[4px]';
-
-  const navigationLinksInfor = [
-    {
-      id: 1,
-      linkName: 'Home',
-      className: linkClassname,
-      href: '/',
-    },
+  const linksInfor: {
+    id?: number;
+    linkName?: string;
+    className?: string;
+    href?: string;
+    classname?: string;
+    dropDownComponent?: JSX.Element;
+  }[] = [
     {
       id: 2,
       dropDownComponent: (
         <DropdownMenu key={2} dropdownItemInforArr={dropdownItemInforArr} />
       ),
     },
-    {
-      id: 3,
-      linkName: 'Products',
-      className: linkClassname,
-      href: '/product',
-    },
-    {
-      id: 4,
-      linkName: 'Blogs',
-      className: linkClassname,
-      href: '/blog',
-    },
-
-    {
-      id: 5,
-      linkName: 'Orders',
-      className: linkClassname,
-      href: '/order-history',
-    },
-
-    {
-      id: 6,
-      linkName: 'Cart',
-      className: linkClassname,
-      href: '/shopping-cart',
-    },
+    ...navigationLinksInfor,
   ];
 
-  return (
-    <div className="mb-8 flex flex-col gap-2 p-4 text-sm text-gray-900 md:block md:p-8 lg:mb-12 lg:p-12 xl:mx-auto xl:max-w-7xl">
-      <nav className="flex h-8 items-center justify-between overflow-visible bg-white">
-        <Link href="/" className="-m-1.5 p-1.5">
-          <Image
-            className="h-20 w-auto md:h-28"
-            src={glowlyLab}
-            alt="logo"
-            priority
-            placeholder="blur"
-          />
-        </Link>
+  const pathname = usePathname();
 
-        {/* table and desktop navigation links */}
-        <div className="hidden items-center sm:flex sm:gap-6 lg:gap-12 xl:gap-16 [&>a]:font-semibold">
-          {navigationLinksInfor.map((navigationLinkInfor) => {
-            if (navigationLinkInfor.id === 2) {
-              return navigationLinkInfor.dropDownComponent;
+  return (
+    <div className={`${pathname === '/admin' ? 'bg-gray-900' : ''}`}>
+      <div
+        className={`mb-8 flex flex-col gap-2 p-4 text-sm text-gray-900 md:p-8 lg:mb-12 lg:p-12 xl:mx-auto xl:max-w-7xl ${
+          pathname === '/admin' ? '!mb-0 bg-gray-900' : ''
+        }`}
+      >
+        <nav
+          className={`flex h-8 items-center justify-between overflow-visible ${
+            pathname === '/admin' ? 'bg-gray-900' : 'bg-white'
+          }`}
+        >
+          <Link href="/" className="-m-1.5 p-1.5">
+            {pathname === '/admin' ? (
+              <p className="font-dancingScript text-3xl font-bold text-white">
+                Glowy
+              </p>
+            ) : (
+              <Image
+                className="h-20 w-auto md:h-28"
+                src={glowlyLab}
+                alt="logo"
+                priority
+                placeholder="blur"
+              />
+            )}
+          </Link>
+
+          {/* table and desktop navigation links */}
+          <div
+            className={`hidden items-center sm:flex sm:gap-6 lg:gap-12 xl:gap-16 [&>a]:font-semibold ${
+              pathname === '/admin' ? 'bg-gray-900 text-white' : ''
+            }`}
+          >
+            {linksInfor.map((navigationLinkInfor) => {
+              if (navigationLinkInfor.id === 2) {
+                return navigationLinkInfor.dropDownComponent;
+              }
+              return (
+                <Link
+                  className={`${navigationLinkInfor.className as string} ${
+                    pathname === '/admin' ? 'hover:border-white' : ''
+                  }`}
+                  href={navigationLinkInfor.href as string}
+                  key={navigationLinkInfor.id}
+                >
+                  {navigationLinkInfor.linkName}
+                </Link>
+              );
+            })}
+          </div>
+
+          <div className={`flex w-[109px] justify-end transition-all`}>
+            {!userProfile && !isLoading && (
+              <Link
+                href="/api/auth/login"
+                className="group relative ml-auto h-[25px] w-[88px] font-semibold"
+              >
+                <span className="group absolute left-0 top-0 z-[1] flex h-full w-full items-center justify-center gap-2 group-hover:text-white">
+                  <span>Log in</span>
+                  <span className="group-hover:hidden">&rarr;</span>
+                  <ArrowLongRightIcon className="hidden h-5 group-hover:block" />
+                </span>
+
+                <span className="absolute left-0 top-0 z-0 h-full w-0 rounded-lg bg-gray-900 transition-all group-hover:w-full"></span>
+              </Link>
+            )}
+
+            {userProfile && !isLoading && (
+              <div className="flex items-center gap-4">
+                <SimpleMenuComponent
+                  avatarSrc={userProfile.imgUrl}
+                  username={userProfile.name}
+                />
+                <Link
+                  href="/shopping-cart"
+                  className="group flex items-end gap-1"
+                >
+                  <ShoppingBagIcon
+                    className={`h-7 text-gray-400 transition-all group-hover:animate-pulse ${
+                      pathname === '/admin'
+                        ? 'group-hover:text-white'
+                        : 'group-hover:text-gray-600'
+                    }`}
+                  />
+                  <p
+                    className={`text-lg font-medium text-gray-400 transition-all group-hover:animate-pulse group-hover:text-gray-600 ${
+                      pathname === '/admin'
+                        ? 'group-hover:text-white'
+                        : 'group-hover:text-gray-600'
+                    }`}
+                  >
+                    {productsInCart.length > 0
+                      ? calculateTotalProducts(productsInCart)
+                      : 0}
+                  </p>
+                </Link>
+              </div>
+            )}
+
+            {isLoading && (
+              <ThreeDots
+                visible={true}
+                height="50"
+                width="50"
+                color={pathname === '/admin' ? 'white' : 'black'}
+                radius="9"
+              />
+            )}
+          </div>
+        </nav>
+
+        {/* mobile navigation links */}
+        <div
+          className={`mx-auto flex items-center gap-4 sm:hidden [&>a]:font-semibold ${
+            pathname === '/admin' ? 'bg-gray-900 text-white' : ''
+          }`}
+        >
+          {linksInfor.map((linkInfor) => {
+            if (linkInfor.id === 2) {
+              return linkInfor.dropDownComponent;
             }
             return (
               <Link
-                className={navigationLinkInfor.className as string}
-                href={navigationLinkInfor.href as string}
-                key={navigationLinkInfor.id}
+                className={linkInfor.className}
+                href={linkInfor.href as string}
+                key={linkInfor.id}
               >
-                {navigationLinkInfor.linkName}
+                {linkInfor.linkName}
               </Link>
             );
           })}
         </div>
-
-        <div className={`flex w-[109px] justify-end transition-all`}>
-          {!userProfile && !isLoading && (
-            <Link
-              href="/api/auth/login"
-              className="group relative ml-auto h-[25px] w-[88px] font-semibold"
-            >
-              <span className="group absolute left-0 top-0 z-[1] flex h-full w-full items-center justify-center gap-2 group-hover:text-white">
-                <span>Log in</span>
-                <span className="group-hover:hidden">&rarr;</span>
-                <ArrowLongRightIcon className="hidden h-5 group-hover:block" />
-              </span>
-
-              <span className="absolute left-0 top-0 z-0 h-full w-0 rounded-lg bg-gray-900 transition-all group-hover:w-full"></span>
-            </Link>
-          )}
-
-          {userProfile && !isLoading && (
-            <div className="flex items-center gap-4">
-              <SimpleMenuComponent
-                avatarSrc={userProfile.imgUrl}
-                username={userProfile.name}
-              />
-              <Link
-                href="/shopping-cart"
-                className="group flex items-end gap-1"
-              >
-                <ShoppingBagIcon className="h-7 text-gray-400 transition-all group-hover:animate-pulse group-hover:text-gray-600" />
-                <p className="text-lg font-medium text-gray-400 transition-all group-hover:animate-pulse group-hover:text-gray-600">
-                  {productsInCart.length > 0
-                    ? calculateTotalProducts(productsInCart)
-                    : 0}
-                </p>
-              </Link>
-            </div>
-          )}
-
-          {isLoading && (
-            <ThreeDots
-              visible={true}
-              height="50"
-              width="50"
-              color="black"
-              radius="9"
-            />
-          )}
-        </div>
-      </nav>
-
-      {/* mobile navigation links */}
-      <div className="mx-auto flex items-center gap-4 sm:hidden [&>a]:font-semibold">
-        {navigationLinksInfor.map((navigationLinkInfor) => {
-          if (navigationLinkInfor.id === 2) {
-            return navigationLinkInfor.dropDownComponent;
-          }
-          return (
-            <Link
-              className={navigationLinkInfor.className}
-              href={navigationLinkInfor.href as string}
-              key={navigationLinkInfor.id}
-            >
-              {navigationLinkInfor.linkName}
-            </Link>
-          );
-        })}
       </div>
     </div>
   );
