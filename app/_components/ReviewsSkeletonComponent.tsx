8ed30b3a84a717a6533bfea5a3d0b065e018b338
Simon Tran <thanhnhantran1501@gmail.com>
2024-05-04 07:21:33 -0700

    fix: fix bugs in customer review component

--- app/_components/ReviewsSkeletonComponent.tsx
+++ app/_components/ReviewsSkeletonComponent.tsx
@@ -2,11 +2,11 @@
 
 export default function ReviewsSkeletonComponent() {
   return (
-    <ul className="max-h-[700px] w-full animate-pulse list-none overflow-auto lg:max-h-[505px] [&>div]:border-b">
+    <ul className="max-h-[700px] w-full animate-pulse list-none overflow-auto lg:max-h-[505px]">
       {[1, 2].map((ele) => (
         <li
           key={ele}
-          className="flex flex-col gap-4 px-4 py-8 last:border-none last:pb-0 lg:first:pt-0"
+          className="flex flex-col gap-4 border-b px-4 py-8 last:border-none last:pb-0 lg:first:pt-0"
         >
           <div className="flex w-full items-center gap-4">
             <div className="size-11 rounded-full bg-gray-200"></div>
