6b6b9c636be8e4acb40e578c22f80f73a57cb995
Simon Tran <thanhnhantran1501@gmail.com>
2024-05-05 13:13:13 -0700

    fix: decrease the size of the image in introduction component

--- app/_components/UserFeedbackComponent.tsx
+++ app/_components/UserFeedbackComponent.tsx
@@ -3,7 +3,7 @@ import { hardCodedUserFeedback } from '../utils/utils';
 
 export default function UserFeedbackComponent() {
   return (
-    <div className="xl:mx-auto xl:max-w-7xl">
+    <div className="cursor-default xl:mx-auto xl:max-w-7xl">
       <h3 className="px-4 md:px-8 lg:px-12">What people say</h3>
       <InfiniteMovingCards
         items={hardCodedUserFeedback}
