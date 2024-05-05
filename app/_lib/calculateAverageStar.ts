67f9114e9927c415cd3b2b27b99493ae505cd227
Simon Tran <thanhnhantran1501@gmail.com>
2024-05-05 14:05:21 -0700

    fix: fix the calculateAverageStar function

--- app/_lib/calculateAverageStar.ts
+++ app/_lib/calculateAverageStar.ts
@@ -4,6 +4,7 @@ import { Review } from '@prisma/client';
 export const calculateAverageStar = function (
   productReviews: (Review & { user: { name: string; imgUrl: string } })[]
 ): number | undefined {
+  console.log(productReviews);
   try {
     const starCounts: number[] = [0, 0, 0, 0, 0];
 
@@ -15,10 +16,9 @@ export const calculateAverageStar = function (
 
     const totalReviews = productReviews.length;
 
-    const sum = starCounts.reduce(
-      (accumulator, currentValue) =>
-        accumulator + currentValue * (starCounts.indexOf(currentValue) + 1)
-    );
+    const sum = starCounts.reduce((accumulator, currentValue, currentIndex) => {
+      return accumulator + currentValue * (currentIndex + 1);
+    });
 
     return Math.round(sum / totalReviews);
   } catch (e: any) {
