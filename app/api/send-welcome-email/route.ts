34d402a0e85c82f745e578eab19619f8dc23cacc
pingpongdoctor <thanhnhantran1501@gmail.com>
2026-08-17 01:49:52 -0700

    add console.log to fix bugs in sender route

--- app/api/send-welcome-email/route.ts
+++ app/api/send-welcome-email/route.ts
@@ -9,18 +9,29 @@ export async function POST(req: Request) {
   const key = headerList.get('Authorization')?.split(' ')[1];
 
   if (key !== process.env.ROUTE_API_KEY) {
+    console.log('Wrong api key');
     return NextResponse.json({ message: 'Wrong api key' }, { status: 401 });
   }
+
+  console.log('api key is valid');
 
   const { from, to, subject, recipient_name }: { [index: string]: string } =
     await req.json();
 
   if (!from || !to || !subject || !recipient_name) {
+    console.log('Miss data');
     return NextResponse.json(
       { message: 'Miss required data' },
       { status: 400 }
     );
   }
+
+  console.log('📦 Payload received:', {
+    from,
+    to,
+    subject,
+    recipient_name,
+  });
 
   const msg = {
     to,
@@ -33,13 +44,15 @@ export async function POST(req: Request) {
   };
 
   try {
+    console.log('📧 About to call SendGrid');
     await sgMail.send(msg);
+    console.log('✅ SendGrid email sent');
     return NextResponse.json(
       { message: 'email has been sent' },
       { status: 200 }
     );
   } catch (err: any) {
-    console.log('Internal server error' + err);
+    console.error('SendGrid error:', err);
     return NextResponse.json(
       { message: err.message },
       { status: err.statusCode || 500 }
